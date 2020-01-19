bcbe178c057aa20f4ab30c86e2ae2c6530d03d01
osmanhajiyev <osman.hajiyev@gmail.com>
2020-01-19 00:45:08 -0800

    player can deal random cards

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+
+    @IBOutlet weak var rightImageView: UIImageView!
+
+    @IBOutlet weak var leftScoreLabel: UILabel!
+
+    @IBOutlet weak var rightScoreLabel: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func dealTapped(_ sender: Any) {
+
+        let leftNumber = Int.random(in: 2...14)
+
+        let rightNumber = Int.random(in: 2...14)
+
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+
+    }
 
 }
 
