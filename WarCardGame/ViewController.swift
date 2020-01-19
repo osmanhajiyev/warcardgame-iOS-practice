d55bf67719b7a6697a81dd4b0f0ca450c21a59f9
osmanhajiyev <osman.hajiyev@gmail.com>
2020-01-19 12:01:37 -0800

    warcard game finished, feel free to play!

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var rightScoreLabel: UILabel!
 
+    var leftScore = 0
+    var rightScore = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,13 +28,27 @@ class ViewController: UIViewController {
 
     @IBAction func dealTapped(_ sender: Any) {
 
+        // Randomize the numbers
         let leftNumber = Int.random(in: 2...14)
-
         let rightNumber = Int.random(in: 2...14)
 
+        // Update the image views
         leftImageView.image = UIImage(named: "card\(leftNumber)")
-
         rightImageView.image = UIImage(named: "card\(rightNumber)")
+
+        // Compare the random numbers
+        if leftNumber > rightNumber {
+            // left side wins
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber {
+            // right side wins
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        } else {
+            // Tie
+        }
 
     }
 
