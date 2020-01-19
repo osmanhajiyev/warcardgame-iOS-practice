68166b6ec02b3cc5b2c1792f86d077a7a11c0d82
osmanhajiyev <osman.hajiyev@gmail.com>
2020-01-18 23:47:09 -0800

    Committing initial work

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Osman Hajiyev on 1/18/20.
+//  Copyright © 2020 Osman Hajiyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
